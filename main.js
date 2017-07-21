8dc11862d48ef02edaf7a0e83311338c2f99dbab
Leonard <leonard.horne@gmail.com>
2017-07-21 14:38:06 -0400

    Iterate over data to create templates for each recipe.

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
-let url = 'https://crossorigin.me/http://www.recipepuppy.com/api?q='
+// Declaring variables - url for data fetch; searchForm and searchInput to
+let url = 'https://crossorigin.me/http://www.recipepuppy.com/api?q=';
+let searchForm = document.getElementById('search');
+let searchInput = searchForm.value;
+let main = document.querySelector('main')
 
-fetch(url).then(
-  function(response) {
-    if (response.status !== 200) {
-          console.log(response.status);
-          return;
-    }
-    response.json().then(function(data) {
-      let searchForm = document.getElementById('search');
+document.querySelector('form.search').addEventListener('submit', function(e) {
+//prevent the normal submission of the form
+  e.preventDefault();
+    let newUrl = `${url}${searchForm.value}`;
+    console.log(newUrl);
 
-      document.querySelector('form.search').addEventListener('submit', function(e) {
-        //prevent the normal submission of the form
-        e.preventDefault();
-        console.log(searchForm.value);
+  fetch(newUrl)
+    .then(function(response) {
+      if (response.status !== 200) {
+            console.log(response.status);
+            return;
+          }
+          response.json().then(function(data) {
+            console.log(data);
+            let recipeContainer = ''
+            data.results.forEach(function(result) {
+              if (result.thumbnail === '') {
+                result.img = ''
+              };
+              let template =
+              `<section>
+                <a href='${result.href}'><p>${result.title}</p></a>
+                <img src='${result.thumbnail}'>
+               </section>
+              `
+              ;
+              recipeContainer += template;
+              console.log(recipeContainer);
+            });
+            main.innerHTML = recipeContainer;
+
       });
+    })
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
     });
-  }
-)
-.catch(function(err) {
-  console.log("Fetch Error :-S", err);
 });
